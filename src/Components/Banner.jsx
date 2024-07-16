c1c1d43438d0dd935a26ae86dc9ae866a66b67f8
apurba-sarkar <sarkar.apurba@hotmail.com>
2024-07-16 15:33:01 +0530

    website optimization

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,26 +1,78 @@
-// import main from "main.jpg"
+// // import main from "main.jpg"
 import "./header.css";
 import "./style.css";
 import { Application } from "@splinetool/runtime";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+// const Banner = () => {
+//   const canvasRef = useRef(null);
+
+//   useEffect(() => {
+//     const canvas = canvasRef.current;
+//     const app = new Application(canvas);
+
+//     app.load("https://prod.spline.design/yStt4MBzAqchxgEu/scene.splinecode");
+
+//     return () => {};
+//   }, []);
+//   const options = {
+//     speed: 1,
+//   };
+//   return (
+//     <div>
+//       <div className="banner">
+//         <canvas ref={canvasRef} id="canvas3d" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
+
+
 const Banner = () => {
   const canvasRef = useRef(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const app = new Application(canvas);
+    let app;
+    if (isLoaded) {
+      const canvas = canvasRef.current;
+      app = new Application(canvas);
+      app.load("https://prod.spline.design/yStt4MBzAqchxgEu/scene.splinecode");
+    }
 
-    app.load("https://prod.spline.design/yStt4MBzAqchxgEu/scene.splinecode");
+    return () => {
+      if (app) {
+        app.dispose();
+      }
+    };
+  }, [isLoaded]);
 
-    return () => {};
-  }, []);
-  const options = {
-    speed: 1,
+  const handleScroll = () => {
+    const bannerElement = document.querySelector('.banner');
+    const rect = bannerElement.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    
+    if (rect.top <= windowHeight && rect.bottom >= 0) {
+      setIsLoaded(true);
+      window.removeEventListener('scroll', handleScroll);
+    }
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Initial check in case the element is already in view
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <div className="banner">
-        <canvas ref={canvasRef} id="canvas3d" />
+        {isLoaded && <canvas ref={canvasRef} id="canvas3d" />}
       </div>
     </div>
   );
